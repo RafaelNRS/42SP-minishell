42a1eabec4edf3dc800f427be7de1f1f1fd55544
ranascim <ranascim@student.42sp.org.br>
2023-04-07 00:42:39 -0300

    Add simple parser

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ranascim <ranascim@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 10:01:47 by ranascim          #+#    #+#             */
-/*   Updated: 2023/03/21 07:47:41 by ranascim         ###   ########.fr       */
+/*   Updated: 2023/04/07 00:23:30 by ranascim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 # include <libft.h>
 # include <minishell.h>
 
-#define MSH_BUFFER_SIZE 1024
+#define MSH_LINE_BUFFER_SIZE 1024
+#define MSH_TOKEN_BUFFER_SIZE 64
+#define MSH_TOKEN_DELIMITER " \t\r\n\a"
 
 #endif
--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ranascim <ranascim@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 07:31:27 by ranascim          #+#    #+#             */
-/*   Updated: 2023/03/21 09:10:46 by ranascim         ###   ########.fr       */
+/*   Updated: 2023/04/07 00:41:58 by ranascim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ char *read_cmd_line(void)
 	int *c;
 
 	position = 0;
-	buffer_size = MSH_BUFFER_SIZE;
+	buffer_size = MSH_LINE_BUFFER_SIZE;
 	buffer = malloc(sizeof(char) * buffer_size);
 	c = malloc(sizeof(int) * 1);
 	if (!buffer || !c)
@@ -45,20 +45,55 @@ char *read_cmd_line(void)
 		buffer[position] = c[0];
 		position++;
 	}
+	// Needs a realloc() implementation if the position exceeds the buffer_size
+}
+
+char **msh_split_line(char *line)
+{
+	int buffer_size;
+	//int position;
+	char **tokens;
+	//char **token;
+
+	buffer_size = MSH_TOKEN_BUFFER_SIZE;
+	tokens = malloc(sizeof(char*) * buffer_size);
+	//position = 0;
+	if (!tokens)
+		msh_error(1);
+	// Needs a function to accept multiple delimiters
+	// token = ft_strtok(line, MSH_TOKEN_DELIMITER);
+	tokens = ft_split(line, ' ');
+	// while (token != NULL)
+	// {
+	// 	tokens[position] = token[position];
+	// 	position++;
+	// }
+	// tokens[position] = NULL;
+	return tokens;
+	// Needs a realloc() implementation if the position exceeds the buffer_size
 }
 
 void minishell_loop(void)
 {
 	char *cmd_line;
+	char **tokens;
 	//char **args;
 	int status;
+	int i;
 	
+	i = 0;
 	status = 999;
 	while (status == 999)
 	{
 		ft_printf("\033[1;96mminishell>");
 		cmd_line = read_cmd_line();
 		ft_printf("%s\n",cmd_line);
+		tokens = msh_split_line(cmd_line);
+		while (tokens[i])
+		{
+			ft_printf("Token %d: %s\n",i, tokens[i]);
+			i++;
+		} 
 		status = 0;
 	}
 }
