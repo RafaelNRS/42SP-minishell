9faeba88a63aff90a4d83a66c9f8d6aa6497d3c8
Mariana Yamada <mariana.yamada@rebase.com.br>
2023-04-08 23:57:23 -0300

    temp

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ranascim <ranascim@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: mariana <mariana@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 10:01:47 by ranascim          #+#    #+#             */
-/*   Updated: 2023/04/08 10:05:28 by ranascim         ###   ########.fr       */
+/*   Updated: 2023/04/08 23:57:04 by mariana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,23 @@
 # include <signal.h>
 # include <stdbool.h>
 
+// pq 1024 e 64?
 # define MSH_LINE_BUFFER_SIZE 1024
 # define MSH_TOKEN_BUFFER_SIZE 64
 # define MSH_TOKEN_DELIMITER " \t\r\n\a"
+# define ENV_MAX_SIZE 2
 
+typedef struct  h_item
+{
+    char    *key;
+    char    *value;
+    struct h_item   *next;
+}   h_item;
+
+typedef struct  h_table
+{
+    h_item   **bucket_items;
+    int                 size;
+    int                 count;
+}   h_table;
 #endif
--- src/minishell.c
+++ src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ranascim <ranascim@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: mariana <mariana@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 07:31:27 by ranascim          #+#    #+#             */
-/*   Updated: 2023/04/08 10:08:48 by ranascim         ###   ########.fr       */
+/*   Updated: 2023/04/08 23:56:34 by mariana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ void msh_error(int error_code)
 		write(2,"minishell: Memory allocation error\n",35);
 	else if (error_code == 2)
 		write(2,"minishell: Too many arguments\n",30);
-
+	else if (error_code == 3)
+		write(2, "Bolinha\n", 8);
 	exit(1);
 }
 
@@ -27,6 +28,7 @@ static void sig_handler(int signal)
 	if (signal == SIGINT)
 	{
 		write(1, "\n", 1);
+		// ???
 		rl_replace_line("", 0);
 		rl_on_new_line();
 		rl_redisplay();
@@ -38,9 +40,7 @@ static void	read_cmd_line(char **cmd_line)
 	char *buffer;
 
 	buffer = "\033[1;96mminishell>";
-	//ft_printf("\033[1;96mminishell>");
 	*cmd_line = readline(buffer);
-	//free(buffer);
 }
 
 char **msh_split_line(char *line)
@@ -71,11 +71,10 @@ char **msh_split_line(char *line)
 void minishell_loop(void)
 {
 	char *cmd_line;
-	char **tokens;
-	//char **args;
-	
+
 	while (true)
 	{
+		// ?signal?
 		signal(SIGQUIT, SIG_IGN);
 		signal(SIGINT, sig_handler);
 		read_cmd_line(&cmd_line);
@@ -84,28 +83,100 @@ void minishell_loop(void)
 			free(cmd_line);
 			continue;
 		}
-		// ft_printf("%s\n",cmd_line);
-		// tokens = msh_split_line(cmd_line);
-		// while (tokens[i])
-		// {
-		// 	ft_printf("Token %d: %s\n",i, tokens[i]);
-		// 	i++;
-		// }
 		if (cmd_line)
 			free(cmd_line);
 		exit(0);
 	}
 }
 
+char	*ft_copy_str(char *src)
+{
+	int		size;
+	int		i;
+	char	*copy_src;
 
+	size = 0;
+	size = ft_strlen(src);
+	if (size == 0)
+		return (NULL);
+	i = 0;
+	copy_src = (char *) malloc(sizeof(char) * size + 1);
+	while (src[i])
+	{
+		copy_src[i] = src[i];
+		i++;
+	}
+	copy_src[i] = '\0';
+	return (copy_src);
+}
+
+void	create_new_item(char *var, h_table *table)
+{
+	char	**env_values;
+	h_item	*new_item;
+
+	env_values = ft_split(var, '=');   
+	new_item = (h_item *)ft_calloc(sizeof(h_item), 1);
+	if (!new_item)
+		msh_error(2);
+    new_item->key = ft_copy_str(env_values[0]);
+    new_item->value = ft_copy_str(env_values[1]);
+	free(env_values);
+    new_item->next = NULL;
+	if (!new_item->key || !new_item->value)
+		msh_error(2);
+	table->bucket_items[0] = new_item;
+	printf("key %s\n", table->bucket_items[0]->key);
+	printf("valye %s\n", table->bucket_items[0]->value);
+}
+
+h_table *create_table(int size)
+{
+	h_table			*new_table;
+	int	i;
+
+	new_table = (h_table *)ft_calloc(sizeof(h_table), 1);
+	new_table->size = size;
+	new_table->count = 0;
+	new_table->bucket_items = (h_item **)ft_calloc(sizeof(h_table), size);
+	i = 0;
+	while (i < size)
+	{
+		new_table->bucket_items[i] = NULL;
+		i++;
+	}
+	return (new_table);
+}
+
+void alloc_hash_table(char **env)
+{
+	// int i;
+	int size;
+	h_table *table;
+
+	size = 0;
+	while (env[size])
+		size++;
+	table = create_table(size * 2);
+	// i = 0;
+	create_new_item(env[0], table);
+	// while (env[i])
+}
 
 int main(int argc, char **argv)
 {
+	// h_table *hash_env;
+
 	if (argc > 1 && argv)
 		msh_error(2);
+
 	//TODO: Load config files
-	minishell_loop();
-	
+	alloc_hash_table(__environ);
+
+	// printf("size %d, count %d\n", hash_env->size, hash_env->count);
+
+	// minishell_loop();
+
 	//TODO: Perform cleanup
 	return 0;
 }
